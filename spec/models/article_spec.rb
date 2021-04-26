79254de7154eeb5962a8ca520f97025433d71908
Ramon Echeverria <ramon.l.echeverria@gmail.com>
2021-04-26 12:51:08 -0400

    rspec and factory bot tests

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "tests article object" do 
+    # expect(1).to be_positive
+
+    article = create(:article)
+    expect(article.title).to eq('Sample Article')
+  end
 end
