a05fbf06c7b48785b7d17297ca12293430faa555
Ramon Echeverria <ramon.l.echeverria@gmail.com>
2021-04-26 20:24:48 -0400

    rspec for articles routing, created articles controller

---
+++ spec/routing/articles_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe '/articles routes' do
+    it 'routes to articles#index' do
+        # expect(get '/articles').to route_to(controller: 'articles', action: 'index')
+        expect(get '/articles').to route_to('articles#index')
+    end
+end
