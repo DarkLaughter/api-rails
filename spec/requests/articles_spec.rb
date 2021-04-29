679859e86eeec70604f009f7ad17a1381a9198eb
Ramon Echeverria <ramon.l.echeverria@gmail.com>
2021-04-28 22:56:51 -0400

    rspec pagination tests

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -38,5 +38,19 @@ RSpec.describe ArticlesController do
 
         end
 
+        it 'paginates results' do
+            article1, article2, article3 = create_list(:article, 3)
+            get '/articles', params: { page: {number: 2, size: 1}}
+            expect(json_data.length).to eq(1)
+            expect(json_data.first[:id]).to eq([article2.id])
+        end
+
+        it 'paginates links exist in response' do
+            article1, article2, article3 = create_list(:article, 3)
+            get '/articles', params: { page: {number: 2, size: 1}}
+            expect(json['links'].length).to eq(5)
+            expect(json['links'].keys).to contain_exactly('first', 'prev', 'next', 'last', 'self')
+        end
+
     end
 end
